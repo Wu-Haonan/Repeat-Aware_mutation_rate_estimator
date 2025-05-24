19992f1fd0113db09a677bd2292e4d5352f42cf6
Wu Haonan <hnw.bio@outlook.com>
2025-05-24 00:52:52 -0400

    remove dump step of kmc

--- Mutation_rate_estimator.py
+++ Mutation_rate_estimator.py
@@ -3,9 +3,10 @@ import argparse
 import os
 import csv
 import subprocess
+import tempfile
 from collections import defaultdict
 from sourmash import load_one_signature, load_file_as_signatures
-from scipy.optimize import newton
+from scipy.optimize import newton,brentq
 
 middle_dir = "./middle_results/"
 os.makedirs(middle_dir, exist_ok=True)
@@ -23,16 +24,70 @@ def run_kmc(input_fasta, db_prefix, k):
     
     return db_path
 
+def build_histogram_cpp(db_path):
+    """avoid dump"""
+    
+    cpp_executable = "./cpp/kmc_histogram"
+    
+    if not os.path.exists(cpp_executable):
+        print(f"[ERROR] C++ histogram tool not found: {cpp_executable}")
+        print("[INFO] Please compile it first:")
+        print("  cd cpp && make")
+        return None, None
+    
+    with tempfile.NamedTemporaryFile(mode='w', suffix='.csv', delete=False) as tmp_file:
+        csv_path = tmp_file.name
+    
+    try:
+        cmd = [cpp_executable, db_path, csv_path]
+        print(f"[INFO] Running C++ histogram: {' '.join(cmd)}")
+        
+        result = subprocess.run(cmd, capture_output=True, text=True, check=True)
+        
+        if result.stdout:
+            print(result.stdout)
+        
+        hist = {}
+        total_kmers = 0
+        
+        with open(csv_path, 'r') as f:
+            reader = csv.reader(f)
+            next(reader)  # jump head
+            
+            for row in reader:
+                if len(row) >= 2:
+                    count = int(row[0])
+                    num_kmers = int(row[1])
+                    hist[count] = num_kmers
+                    total_kmers += num_kmers
+        
+        print(f"[INFO] C++ histogram completed: {total_kmers:,} total k-mers")
+        return hist, total_kmers
+        
+    except subprocess.CalledProcessError as e:
+        print(f"[ERROR] C++ histogram failed: {e.stderr}")
+        return None, None
+    
+    except Exception as e:
+        print(f"[ERROR] Failed to process histogram: {e}")
+        return None, None
+    
+    finally:
+        if os.path.exists(csv_path):
+            os.unlink(csv_path)
+
 def dump_kmc_to_txt(db_prefix, output_file="kmc_dump.txt"):
+    """alternative, dump"""
     output_path = os.path.join(middle_dir, output_file)
     cmd = f"kmc_dump {db_prefix} {output_path}"
-    print(f"[INFO] Running: {cmd}")
+    print(f"[INFO] Running fallback dump: {cmd}")
     subprocess.run(cmd, shell=True, check=True)
     
     return output_path
 
 # ---------- Histogram ----------
 def build_histogram_from_kmc_dump(dump_file):
+    """alternative, read dump"""
     hist = defaultdict(int)
     total_kmers = 0
     with open(dump_file) as f:
@@ -83,25 +138,20 @@ def compute_intersection(sig1_file, sig2_file):
         print(f"[ERROR] {e}")
         return None
 
-# def compute_intersection(sig1_file, sig2_file):
-#     s1 = load_one_signature(sig1_file)
-#     s2 = load_one_signature(sig2_file)
-#     mh1 = s1.minhash
-#     mh2 = s2.minhash
-#     intersection_size = mh1.count_common(mh2)
-#     print(f"[INFO] Sketch intersection : {intersection_size}")
-#     return intersection_size
-
 # ---------- Newton Solver ----------
 def solve_histogram_equation(hist, I, total_kmers):
-    def poly(x):
-        return (total_kmers - I) - sum(count * x**i for i, count in hist.items()) 
-    try:
-        root = newton(poly, x0=0.5)
-    except Exception as e:
-        print(f"[ERROR] Newton solver failed: {e}")
-        return None
-    return root
+    if I ==0:
+        return 1
+    else:
+        def poly(x):
+            return (total_kmers - I) - sum(count * x**i for i, count in hist.items()) 
+        try:
+            #root = newton(poly, x0=0.3, tol=1e-15)
+            root = brentq(poly, 0, 1, xtol=1e-15)
+        except Exception as e:
+            print(f"[ERROR] Newton solver failed: {e}")
+            return None
+        return root
 
 # ---------- Cleanup function ----------
 def cleanup_middle_files():
@@ -119,6 +169,7 @@ def main():
     parser.add_argument("--k", type=int, default=31, help="k-mer size")
     parser.add_argument("--theta", type=float, default=1, help="Sketching threshold (default=1)")
     parser.add_argument("--cleanup", action="store_true", help="Clean up middle files after completion")
+    parser.add_argument("--use-dump", action="store_true", help="Force use traditional dump method")
     args = parser.parse_args()
 
     print(f"[INFO] Middle files will be stored in: {middle_dir}")
@@ -127,15 +178,22 @@ def main():
     if args.mode in ["sequence", "mixture"]:
         db_path = run_kmc(args.input1, "kmc_db", args.k)
         
-        dump_file_path = dump_kmc_to_txt(db_path, "kmc_dump.txt")
+        # call kmc_histgram.cpp
+        if not args.use_dump:
+            hist, total_kmers = build_histogram_cpp(db_path)
+        else:
+            hist, total_kmers = None, None
         
-        hist, total_kmers = build_histogram_from_kmc_dump(dump_file_path)
+        # if c++ fail
+        if hist is None:
+            print("[INFO] Falling back to traditional dump method...")
+            dump_file_path = dump_kmc_to_txt(db_path, "kmc_dump.txt")
+            hist, total_kmers = build_histogram_from_kmc_dump(dump_file_path)
         
     elif args.mode == "kmer":
         if not args.dist:
             raise ValueError("--dist must be provided in kmer mode")
         hist, total_kmers = read_histogram_csv(args.dist)
-
 
     sig1_path = sketch(args.input1, args.k, scaled, "sig1.sig")
     sig2_path = sketch(args.input2, args.k, scaled, "sig2.sig")
@@ -151,7 +209,7 @@ def main():
         q = solve_histogram_equation(hist, I, total_kmers)
         r = 1 - (1 - q) ** (1/args.k)
     
-    print(f"[RESULT] Estimated : {r}")
+    print(f"[RESULT] Estimated r : {r}")
     
     # option: clean middle data
     if args.cleanup:
--- README.md
+++ README.md
@@ -1,14 +1,138 @@
 # Repeat-Aware_mutation_rate_estimator
-This repository builds upon the ideas from [this work](https://github.com/medvedevgroup/Repeat-Aware_Substitution_Rate_Estimator/tree/main), with significant optimizations and extensions. It enables faster computation of mutation rates (genomic distances) between two whole genomes, leveraging KMC and other mature tools.
+
+This tool estimates the substitution rate between two set of sequences (e.g., two genomes, or raw sequencing reads). Our method is robust to the input sequences with high repetitiveness.
+
+This repository builds upon the ideas from [this work](https://github.com/medvedevgroup/Repeat-Aware_Substitution_Rate_Estimator/tree/main), with significant optimizations. It enables faster computation of mutation rates (genomic distances) between two whole genomes, leveraging KMC and other mature tools. 
+
+---
+
+## Installation
+
+We recommend using Conda to set up the environment:
 
 
+Install all the packages based on `requirements.txt`
 
-# Usage
-
-## Requirement 
-
-```
+```bash
 conda create -n <env_name> python=3.10 --file requirements.txt -c conda-forge -c bioconda -y
 conda activate <env_name>
 ```
 
+---
+
+## Example Usage
+
+We provide a small example under `example_data/`.
+
+### Run from sequence mode (two FASTA files):
+
+```bash
+python Mutation_rate_estimator.py \
+  --mode sequence \
+  --input1 example_data/origin_seq.fasta \
+  --input2 example_data/mutated_seq.fasta \
+  --k 31 \
+  --theta 0.01
+```
+
+### Run from mixture mode (one FASTA file + k-mer FASTA):
+
+```bash
+python Mutation_rate_estimator.py \
+  --mode mixture \
+  --input1 example_data/origin_seq.fasta \
+  --input2 example_data/mutated_kmers.fasta \
+  --k 31 \
+  --theta 0.01
+```
+
+### Run from k-mer mode (two k-mer FASTA + histogram):
+
+```bash
+python Mutation_rate_estimator.py \
+  --mode kmer \
+  --input1 example_data/origin_kmers.fasta \
+  --input2 example_data/mutated_kmers.fasta \
+  --dist example_data/histogram.csv \
+  --k 31 \
+  --theta 0.01
+```
+
+---
+
+## Usage
+
+### Inputs
+
+#### Sequence mode
+
+Provide two complete sequences (could include multiple sequences) in separate FASTA files:
+
+```bash
+python Mutation_rate_estimator.py \
+  --mode sequence \
+  --input1 seq1.fasta \
+  --input2 seq2.fasta \
+  --k 31
+```
+
+#### Mixture mode
+
+Provide sequences in FASTA format (could include multiple sequences) and one set of k-mers in FASTA format:
+
+```bash
+python Mutation_rate_estimator.py \
+  --mode mixture \
+  --input1 seq1.fasta \
+  --input2 set2.fasta \
+  --k 31
+```
+
+#### K-mer mode
+
+Provide two sets of k-mers in FASTA format and a histogram CSV file. The histogram file should contain:
+
+```csv
+count,num_kmers
+1,12345
+2,6789
+...
+```
+
+Then run:
+
+```bash
+python Mutation_rate_estimator.py \
+  --mode kmer \
+  --input1 set1.fasta \
+  --input2 set2.fasta \
+  --dist occ.csv \
+  --k 31
+```
+
+### Using sketching
+
+You can use the `--theta` parameter to apply FracMinHash sketching to approximate the intersection size between two k-mer sets.
+
+- `theta` controls the sampling rate (default = 0.01). 
+- The value must be in the range `(0, 1]`.
+- For example, `--theta 0.01` retains approximately 1% of the hashed k-mers.
+
+Example:
+
+```bash
+python Mutation_rate_estimator.py \
+  --mode sequence \
+  --input1 genome1.fasta \
+  --input2 genome2.fasta \
+  --k 31 \
+  --theta 0.01
+```
+
+
+
+Note: Sketching could significantly accelerate large-scale computation. In this repository, we aim to estimate mutation rates between large-scale sequence datasets, such as reference genomes or whole-genome sequencing reads. To enable efficient computation, we use [sourmash](https://sourmash.readthedocs.io/en/latest/index.html) to implement FracMinHash sketching.
+
+Note that sourmash requires a `--scaled` parameter of at least 100, which corresponds to a `theta` value of at most $0.01$ here. If you provide a larger `theta`, sourmash will automatically enforce it to be 0.01.
+
+If you prefer exact intersection computation or are working with smaller datasets, we recommend using the original version [Repeat-Aware_Substitution_Rate_Estimator](https://github.com/medvedevgroup/Repeat-Aware_Substitution_Rate_Estimator). However, for large-scale data, we highly recommend using this sketch-based tool for its speed and scalability.
